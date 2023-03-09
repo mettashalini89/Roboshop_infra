7c381eb95c8e868323d6facefe45e3b533ada28f
mettashalini89@gmail.com <CatMatDog@123>
2023-03-08 19:50:53 -0700

    parameters config

--- aws-parameters/state.tf
+++ aws-parameters/state.tf
@@ -4,9 +4,9 @@ terraform {
 
 resource "aws_ssm_parameter" "parameter" {
   count = length(var.parameters)
-  name  = "var.parameters[count.index].name"
-  type  = "var.parameters[count.index].type"
-  value = "var.parameters[count.index].value"
+  name  = var.parameters[count.index].name
+  type  = var.parameters[count.index].type
+  value = var.parameters[count.index].value
 }
 
 variable "parameters" {}
