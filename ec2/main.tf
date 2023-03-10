bfb1d70eff529dcd8cf29bee8a2bb1f904b5aa41
mettashalini89@gmail.com <CatMatDog@123>
2023-03-10 15:19:45 -0700

    adding env variable

--- ec2/main.tf
+++ ec2/main.tf
@@ -18,7 +18,7 @@ resource "null_resource" "provisioner" {      #Give provisioner saperately so it
     }
 
     inline = [
-      "ansible-pull -i localhost, -U https://github.com/mettashalini89/roboshop-ansible roboshop.yml -e role_name=${var.component} -e env=${env}"
+      "ansible-pull -i localhost, -U https://github.com/mettashalini89/roboshop-ansible roboshop.yml -e role_name=${var.component} -e env=${var.env}"
 
     ]
 
