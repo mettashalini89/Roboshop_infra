770e63eb0591fc68e385c677f4d34114534a49fe
mettashalini89@gmail.com <CatMatDog@123>
2023-03-10 00:43:54 -0700

    policy adding

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,11 +1,3 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "devops-practice-with-ansible"
-  owners           = [data.aws_caller_identity.current.account_id]
-}
-
 resource "aws_instance" "ec2"{  #first lable is from terraform and second lable is any can be given by user for his ref
   ami = data.aws_ami.ami.image_id
   instance_type = var.instance_type
@@ -66,10 +58,32 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.ec2.private_ip]
 }
 
-variable "component" {}
-variable "instance_type" {}
-
-variable "env" {
-  default = "dev"
+resource "aws_iam_policy" "ssm-policy" {
+  name        = "${var.env}-${var.component}-ssm"
+  path        = "/"
+  description = "${var.env}-${var.component}-ssm"
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:780395309002:parameter/${var.env}.${var.component}*"
+      },
+      {
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "ssm:DescribeParameters",
+        "Resource": "*"
+      }
+    ]
+  })
 }
+
 
