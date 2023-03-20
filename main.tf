a62fcaf634eae17473eead60322aac5ed31c049a
mettashalini89@gmail.com <CatMatDog@123>
2023-03-20 11:57:02 -0600

    vpc creation

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "vpc" {
   env = var.env
   source = "git::https://github.com/mettashalini89/tf_module_vpc.git"
   for_each = var.vpc
-  vpc_cidr = each.value["cidr"]
+  vpc_cidr = each.value["vpc_cidr"]
 }
