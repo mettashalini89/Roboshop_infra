01920355853c328dd871a2cb5078c88ea659d992
mettashalini89@gmail.com <CatMatDog@123>
2023-03-22 01:03:08 -0600

    mongodb config as aws service

--- main.tf
+++ main.tf
@@ -10,18 +10,18 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
-module "docdb" {
-  env = var.env
-  source = "git::https://github.com/mettashalini89/tf_module_docdb.git"
-  for_each = var.docdb
-  tags = var.tags
-  engine = each.value["engine"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot = each.value["skip_final_snapshot"]
+#module "docdb" {
+  #env = var.env
+  #source = "git::https://github.com/mettashalini89/tf_module_docdb.git"
+  #for_each = var.docdb
+  #tags = var.tags
+  #engine = each.value["engine"]
+  #backup_retention_period = each.value["backup_retention_period"]
+  #preferred_backup_window = each.value["preferred_backup_window"]
+  #skip_final_snapshot = each.value["skip_final_snapshot"]
 
-  engine_version = each.value["engine_version"]
-  subnet_ids = local.db_subnet_ids
+  #engine_version = each.value["engine_version"]
+  #subnet_ids = local.db_subnet_ids
 
-}
+#}
 
