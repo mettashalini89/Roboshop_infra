8cefcd746b75014bed819c66acd60bb9c0098301
mettashalini89@gmail.com <CatMatDog@123>
2023-03-22 15:58:52 -0600

    mongodb config as aws service adding subnet groups

--- locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-
-  db_subnet_ids = tolist([module.vpc["vpc"].private_subnets["db-az1"].id, module.vpc["vpc"].private_subnets["db-az2"].id])
-  app_subnet_ids = tolist([module.vpc["vpc"].private_subnets["app-az1"].id, module.vpc["vpc"].private_subnets["app-az2"].id])
-  web_subnet_ids = tolist([module.vpc["vpc"].private_subnets["web-az1"].id, module.vpc["vpc"].private_subnets["web-az2"].id])
-
-}
