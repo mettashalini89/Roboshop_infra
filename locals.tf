019f9dde557d94c945e1b2be5989373d530b4ba9
mettashalini89@gmail.com <CatMatDog@123>
2023-03-22 01:04:17 -0600

    mongodb config as aws service

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
-locals {
-  db_subnet_ids = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id] )
-}
+#locals {
+#  db_subnet_ids = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id] )
+#}
