3a64056f3f0459a6e07199403f54981eac33a21b
mettashalini89@gmail.com <CatMatDog@123>
2023-03-22 15:00:42 -0600

    mongodb config as aws service adding subnet groups

--- main.tf
+++ main.tf
@@ -10,18 +10,17 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
-#module "docdb" {
-  #env = var.env
-  #source = "git::https://github.com/mettashalini89/tf_module_docdb.git"
-  #for_each = var.docdb
-  #tags = var.tags
-  #engine = each.value["engine"]
-  #backup_retention_period = each.value["backup_retention_period"]
-  #preferred_backup_window = each.value["preferred_backup_window"]
-  #skip_final_snapshot = each.value["skip_final_snapshot"]
+module "docdb" {
+  env = var.env
+  source = "git::https://github.com/mettashalini89/tf_module_docdb.git"
+  for_each = var.docdb
+  tags = var.tags
+  engine = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot = each.value["skip_final_snapshot"]
+  subnet_ids = local.db_subnet_ids
 
-  #engine_version = each.value["engine_version"]
-  #subnet_ids = local.db_subnet_ids
-
-#}
+}
 
