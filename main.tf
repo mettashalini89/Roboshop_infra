43ba9ffefb0300f6e36dc9a2fafe4ecedff5169d
mettashalini89@gmail.com <CatMatDog@123>
2023-03-23 12:55:24 -0600

    rabbitmq config using aws service ec2 spot instance, adding subnet groups

--- main.tf
+++ main.tf
@@ -22,8 +22,8 @@ module "docdb" {
   preferred_backup_window = each.value["preferred_backup_window"]
   skip_final_snapshot = each.value["skip_final_snapshot"]
   subnet_ids = local.db_subnet_ids
-  no_of_instances = each.value["no_of_instances"]
-  instance_class = each.value["instance_class"]
+  #no_of_instances = each.value["no_of_instances"]
+  #instance_class = each.value["instance_class"]
 }
 
 module "rds" {
