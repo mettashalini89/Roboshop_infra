f87a13941ae0f537c66935ca351da9a7f8a0120a
mettashalini89@gmail.com <CatMatDog@123>
2023-03-26 17:51:30 -0600

    interaction btwn app and alb

--- main.tf
+++ main.tf
@@ -95,6 +95,7 @@ module "app" {
   min_size = each.value["min_size"]
   subnets = lookup(local.subnet_ids, each.value["subnet_name"], null)
   port = each.value["port"]
+  alb = each.value["alb"]
   allow_app_to = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
   alb_dns_name = lookup(lookup(lookup(module.alb, each.value["alb"], null ), "alb", null), "dns_name", null)
 }
