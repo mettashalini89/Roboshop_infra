2fd02cc8267b0ba690ae20480514c5a2479cc8f6
mettashalini89@gmail.com <CatMatDog@123>
2023-04-24 11:07:58 -0600

    jenkins practice

--- main.tf
+++ main.tf
@@ -113,9 +113,6 @@ module "app" {
   listner_arn = lookup(lookup(lookup(module.alb, each.value["alb"], null ), "listner", null), "arn", null)
 }
 
-/*output "elasticahe" {
-  value = module.elasticache
-}*/
 
 /*
 
