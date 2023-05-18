caf0b4f4afa85ac6cd5d6e0ec8e1769c8d323d74
mettashalini89@gmail.com <CatMatDog@123>
2023-05-17 23:36:30 -0600

    kubernets practice

--- data.tf
+++ data.tf
@@ -7,6 +7,13 @@ data "aws_ami" "ami" {
 }
 */
 
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+
 data "aws_ssm_parameter" "ssh_pass"{
   name = "${var.env}.ssh.pass"
 }
